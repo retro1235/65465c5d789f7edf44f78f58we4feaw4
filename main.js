9a2b43190b6a6077ba07d22f0cad498a166642e6
retro1235 <82151737+retro1235@users.noreply.github.com>
2022-08-16 20:52:43 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,53 @@
+song = "";
+song1 = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
+
+function preload()
+{
+    song = loadSound("music.mp3");
+    song1 = loadsound("music2.mp3");
+}
+
+
+function setup() {
+canvas = createCanvas(600,500);
+canvas.center();
+
+video = createCapture(VIDEO);
+video.hide();
+
+poseNet = ml5.poseNet(video, modelLodaded);
+poseNet.on('pose',gotPoses);
+}
+
+function modelLodaded() {
+    console.log('posenet is intialized')
+}
+
+function draw() {
+    image(video, 0,0,600,500 );
+}
+
+function play() {
+    song.setVolume(1);
+    song.rate(1);
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+{
+    console.log(results);
+    leftWristX = results[0].pose.leftWrist.x;
+    leftWristY = results[0].pose.leftWrist.y;
+    console.log("leftWristX = " + leftWristX +"leftWristY = "+leftWristY)
+
+   rightWristX = results[0].pose.rightWrist.x;
+   rightWristY = results[0].pose.rightWrist.y;
+    console.log("rightWristX = " + rightWristX +"rightWristY = "+rightWristY)
+}
+}
+
